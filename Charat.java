e716bbb395141d1af596818044d847b0fef64139
shoba-001 <jakkulashoba@gmail.com>
2022-10-18 12:00:34 +0530

    New Pro2

--- Charat.java
+++ Charat.java
@@ -3,10 +3,12 @@ package class3;
 public class Charat {
 	public static void main(String args[]) {
 		String name = "Shoba";
+		String name2="Manu";
 		char letter = name.charAt(2);
 		char letter1 = name.charAt(name.length()-2);
 		System.out.println(letter);
 		System.out.println(letter1);
+		System.out.println(name2);
 	
 	}
 
